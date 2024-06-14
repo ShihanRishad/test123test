3addd5efc888c9c961c3f8426b353a6338461beb
Shihan Rishad <165176498+ShihanRishad@users.noreply.github.com>
2024-06-14 21:04:10 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, error => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+document.getElementById('changeThemeButton').addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomColor();
+});
